d41dce15475b43737cc245e6eacc927fa3888550
Tan Liwei <236328172@qq.com>
2018-05-27 23:46:42 +0800

    first commit

---
+++ src/main/java/cn/tanlw/java/think/Test.java
@@ -0,0 +1,29 @@
+package cn.tanlw.java.think;
+
+
+/**
+ * https://blog.csdn.net/xiaanming/article/details/8703708/
+ * 测试类
+ * @author xiaanming
+ *
+ */
+public class Test {
+    public static void main(String[]args){
+        /**
+         * new 一个小李
+         */
+        Li li = new Li();
+
+        /**
+         * new 一个小王
+         */
+        Wang wang = new Wang(li);
+
+        /**
+         * 小王问小李问题
+         */
+        wang.askQuestion("1 + 1 = ?");
+    }
+}
+
+
